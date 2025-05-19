b3e399d0ce34aad2e913bfd98ef77b7da5c8736c
Parnia Zare <150398614+pzare2023@users.noreply.github.com>
2025-05-18 20:07:48 -0400

    Update p1.py

--- ML A1/p1.py
+++ ML A1/p1.py
@@ -6,15 +6,9 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
 
-############## FOR EVERYONE ##############
-# Please note that the blanks are here to guide you for this first assignment, but the blanks are  
-# in no way representative of the number of commands/ parameters or length of what should be inputted.
-
 ### PART 1 ###
 # Scikit-Learn provides many popular datasets. The breast cancer wisconsin dataset is one of them. 
-# Write code that fetches the breast cancer wisconsin dataset. 
-# Hint: https://scikit-learn.org/stable/datasets/toy_dataset.html
-# Hint: Make sure the data features and associated target class are returned instead of a "Bunch object".
+
 X, y = datasets.load_breast_cancer(return_X_y=True) #(4 points) 
 
 # Check how many instances we have in the dataset, and how many features describe these instances
@@ -23,13 +17,10 @@ print("There are",X.shape[0], "instances described by", X.shape[1], "features.")
 # Create a training and test set such that the test set has 40% of the instances from the 
 # complete breast cancer wisconsin dataset and that the training set has the remaining 60% of  
 # the instances from the complete breast cancer wisconsin dataset, using the holdout method. 
-# In addition, ensure that the training and test sets # contain approximately the same 
-# percentage of instances of each target class as the complete set.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, stratify=y, random_state = 42)  #(4 points) 
 
 # Create a decision tree classifier. Then Train the classifier using the training dataset created earlier.
 # To measure the quality of a split, using the entropy criteria.
-# Ensure that nodes with less than 6 training instances are not further split
 clf = DecisionTreeClassifier(criterion='entropy', min_samples_split=6)  #(4 points) 
 clf = clf.fit(X_train, y_train)  #(4 points) 
 
